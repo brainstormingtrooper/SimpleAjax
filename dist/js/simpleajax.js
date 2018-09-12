d1e693871a55973fac004abd24accb8ce897c968
Halyson Rezende <halyson97@gmail.com>
2018-09-12 15:14:24 -0300

    Requisicao support method HEAD

--- dist/js/simpleajax.js
+++ dist/js/simpleajax.js
@@ -2,50 +2,52 @@
 
 window.ajaxPromise = function () {
 
-	var req = new XMLHttpRequest();
-	var result = function result(req) {
+  var req = new XMLHttpRequest();
+  var result = function result(req) {
+    if (req.response) {
+      return {
+        data: JSON.parse(req.response),
+        status: req.status
+      };
+    } else {
+      return {
+        status: req.status
+      };
+    }
+  };
 
-		if (req.response) {
-			return {
-				data: JSON.parse(req.response),
-				status: req.status
-			};
-		} else {
-			return {
-				status: req.status
-			};
-		}
-	};
-
-	return function ajaxparams(_ref) {
-		var _ref$method = _ref.method,
-		    method = _ref$method === undefined ? 'GET' : _ref$method,
-		    url = _ref.url,
-		    _ref$data = _ref.data,
-		    data = _ref$data === undefined ? null : _ref$data,
-		    _ref$header = _ref.header,
-		    header = _ref$header === undefined ? {} : _ref$header;
+  return function ajaxparams(_ref) {
+    var _ref$method = _ref.method,
+        method = _ref$method === undefined ? 'GET' : _ref$method,
+        url = _ref.url,
+        _ref$data = _ref.data,
+        data = _ref$data === undefined ? null : _ref$data,
+        _ref$contentType = _ref.contentType,
+        contentType = _ref$contentType === undefined ? 'application/json' : _ref$contentType,
+        _ref$header = _ref.header,
+        header = _ref$header === undefined ? {} : _ref$header;
 
 
-		if (!url) {
-			return Promise.reject('url is required!');
-		}
+    if (!url) {
+      return Promise.reject('url is required!');
+    }
 
-		req.open(method.toUpperCase(), url, true);
+    req.open(method.toUpperCase(), url, true);
+    req.setRequestHeader('Content-Type', contentType);
 
-		for (var head in header) {
-			req.setRequestHeader(head, header[head]);
-		}
+    for (var head in header) {
+      req.setRequestHeader(head, header[head]);
+    }
 
-		req.send(data);
+    req.send(JSON.stringify(data));
 
-		return new Promise(function (resolve, reject) {
+    return new Promise(function (resolve, reject) {
 
-			var success = function success() {
-				return req.status < 400 && req.readyState === 4 ? resolve(result(req)) : reject(result(req));
-			};
+      var success = function success() {
+        return req.status < 400 && req.readyState === 4 ? resolve(result(req)) : reject(result(req));
+      };
 
-			req.addEventListener('load', success, false);
-		});
-	};
+      req.addEventListener('load', success, false);
+    });
+  };
 }();
--- src/js/lib/simpleajax.js
+++ src/js/lib/simpleajax.js
@@ -1,41 +1,40 @@
 window.ajaxPromise = (function(){
 
-	const req = new XMLHttpRequest();
-	const result = req => {
+  const req = new XMLHttpRequest();
+  const result = req => {
+      if(req.response){
+          return {
+              data: JSON.parse(req.response),
+              status: req.status
+            };
+      } else {
+          return {
+              status: req.status
+            };
+      }
+  };
 
-        if(req.response){
-            return {
-                data: JSON.parse(req.response),
-                status: req.status
-            }
-        }else{
-            return {
-                status: req.status
-            }
-        }
+  return function ajaxparams({method = 'GET', url, data = null, contentType = 'application/json', header = {}}) {
+
+    if (!url) {
+      return Promise.reject('url is required!');
+    }
+
+    req.open(method.toUpperCase(), url, true);
+    req.setRequestHeader('Content-Type', contentType);
+
+    for(let head in header){
+      req.setRequestHeader(head, header[head]);
+    }
     
-    };
+    req.send(JSON.stringify(data));
 
-	return function ajaxparams({method = 'GET', url, data = null, header = {}}) {
+    return new Promise((resolve, reject) =>  {
 
-		if (!url) {
-			return Promise.reject('url is required!');
-		}
+      const success = () => (req.status < 400 && req.readyState === 4) ?
+          resolve(result(req)) : reject(result(req));
 
-		req.open(method.toUpperCase(), url, true);
-		
-		for(let head in header){
-			req.setRequestHeader(head, header[head]);
-		}
-		
-		req.send(data);
-
-		return new Promise((resolve, reject) =>  {
-
-			const success = () => (req.status < 400 && req.readyState === 4) ?
-				resolve(result(req)) : reject(result(req));
-
-			req.addEventListener('load', success, false);
-		});
-	};
+      req.addEventListener('load', success, false);
+    });
+  };
 })();
