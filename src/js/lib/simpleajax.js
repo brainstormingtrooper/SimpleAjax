b16e054d62282090f0d8eb10ade7cdee1b45cf82
Halyson Rezende <halyson97@gmail.com>
2018-09-26 15:14:03 -0300

    Add listener onerror e onabort

--- src/js/lib/simpleajax.js
+++ src/js/lib/simpleajax.js
@@ -1,40 +1,44 @@
 window.ajaxPromise = (function(){
 
-  const req = new XMLHttpRequest();
-  const result = req => {
-      if(req.response){
-          return {
-              data: JSON.parse(req.response),
-              status: req.status
-            };
-      } else {
-          return {
-              status: req.status
-            };
-      }
-  };
+	const req = new XMLHttpRequest();
+	const result = req => {
+		if(req.response){
+			return {
+				data: JSON.parse(req.response),
+				status: req.status
+			};
+		} else {
+			return {
+				status: req.status
+			};
+		}
+	};
 
-  return function ajaxparams({method = 'GET', url, data = null, contentType = 'application/json', header = {}}) {
+	return function ajaxparams({method = 'GET', url, data = null, contentType = 'application/json', header = {}}) {
 
-    if (!url) {
-      return Promise.reject('url is required!');
-    }
+		if (!url) {
+			return Promise.reject('url is required!');
+		}
 
-    req.open(method.toUpperCase(), url, true);
-    req.setRequestHeader('Content-Type', contentType);
+		req.open(method.toUpperCase(), url, true);
+		req.setRequestHeader('Content-Type', contentType);
 
-    for(let head in header){
-      req.setRequestHeader(head, header[head]);
-    }
-    
-    req.send(JSON.stringify(data));
+		for(let head in header){
+			req.setRequestHeader(head, header[head]);
+		}
+		
+		req.send(JSON.stringify(data));
 
-    return new Promise((resolve, reject) =>  {
+		return new Promise((resolve, reject) =>  {
 
-      const success = () => (req.status < 400 && req.readyState === 4) ?
-          resolve(result(req)) : reject(result(req));
+			const success = () => (req.status < 400 && req.readyState === 4) ?
+				resolve(result(req)) : reject(result(req));
 
-      req.addEventListener('load', success, false);
-    });
-  };
+			const error = () => (reject(result(req)));
+
+			req.addEventListener('load', success, false);
+			req.addEventListener("error", error, false);
+			req.addEventListener("abort", error, false);
+		});
+	};
 })();
